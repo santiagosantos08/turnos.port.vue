ee4363ef12ef67ba31fda21081fdd752d5dee48c
Santiago <89073340+santiagosantos08@users.noreply.github.com>
2023-07-30 02:14:36 +0000

    save1

--- reservApp/src/App.vue
+++ reservApp/src/App.vue
@@ -8,11 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="C.A.E de C. Bolívar" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Reservar</RouterLink>
+        <RouterLink to="/about">Consultar</RouterLink>
       </nav>
     </div>
   </header>
