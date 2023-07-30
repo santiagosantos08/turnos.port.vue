5476443177d02dfc04de96f6a198b4231816ed94
Santiago <89073340+santiagosantos08@users.noreply.github.com>
2023-07-30 19:45:29 +0000

    added signup view

--- reservApp/src/main.ts
+++ reservApp/src/main.ts
@@ -6,6 +6,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// ----- firebase config -------
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyANiN8PTkZb7RRUqYYWLoncFO-8ZC0rdwk",
+  authDomain: "cecreservas-1237e.firebaseapp.com",
+  projectId: "cecreservas-1237e",
+  storageBucket: "cecreservas-1237e.appspot.com",
+  messagingSenderId: "852907542670",
+  appId: "1:852907542670:web:3c4112a1038c4025c7a1df",
+  measurementId: "G-PKFHWZKG09"
+};
+
+initializeApp(firebaseConfig);
+// ------ firebase -------
+
 const app = createApp(App)
 
 app.use(createPinia())
