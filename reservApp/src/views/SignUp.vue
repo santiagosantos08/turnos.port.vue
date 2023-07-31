ea87df5ddfdf1961eafb62d5916bfe036700b4ac
Santiago <89073340+santiagosantos08@users.noreply.github.com>
2023-07-31 04:05:38 +0000

    codespace

--- reservApp/src/views/SignUp.vue
+++ reservApp/src/views/SignUp.vue
@@ -1,9 +1,11 @@
 <template>
-    <h1>Registrarse</h1>
-    <p><input type="email" placeholder="Email" v-model="email" /></p>
-    <p><input type="password" placeholder="Contraseña" v-model="password" /></p>
-    <p><button @click="register">Crear Cuenta</button></p>
-    <p><button @click="signInWithGoogle">Entrar con Google</button></p>
+    <h1>Registrarse.</h1>
+    <div class="item">
+        <p><input type="email" placeholder="Email" v-model="email" /></p>
+        <p><input type="password" placeholder="Contraseña" v-model="password" /></p>
+        <p><button @click="register">Crear Cuenta</button></p>
+        <p><button @click="signInWithGoogle">Entrar con Google</button></p>
+    </div>
 </template>
 
 <script setup>
@@ -30,3 +32,36 @@
 
     }
 </script>
+
+<style>
+.item {
+  margin-top: 2rem;
+  display: flex;
+  position: relative;
+}
+
+@media (min-width: 1024px) {
+  .item {
+    margin-top: 0;
+    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+  }
+
+  .item:before {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    bottom: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:after {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    top: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+}
+</style>
